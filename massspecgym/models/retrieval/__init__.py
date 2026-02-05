aedd7daa688de381f30a3865ea613f9ca93f131f
harrylaucngd <harrylaucngd@gmail.com>
2026-02-05 10:10:10 -0500

    adding MIST and SpecBridge Evals

--- massspecgym/models/retrieval/__init__.py
+++ massspecgym/models/retrieval/__init__.py
@@ -3,11 +3,15 @@ from .random import RandomRetrieval
 from .deepsets import DeepSetsRetrieval
 from .fingerprint_ffn import FingerprintFFNRetrieval
 from .from_dict import FromDictRetrieval
+from .specbridge import SpecBridgeRetrieval
+from .mist_encoder import MistEncoderRetrieval
 
 __all__ = [
     "RetrievalMassSpecGymModel",
     "RandomRetrieval",
     "DeepSetsRetrieval",
     "FingerprintFFNRetrieval",
-    "FromDictRetrieval"
+    "FromDictRetrieval",
+    "SpecBridgeRetrieval",
+    "MistEncoderRetrieval",
 ]
